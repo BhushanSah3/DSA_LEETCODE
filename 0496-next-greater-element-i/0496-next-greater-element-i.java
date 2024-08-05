4b460e050744fe07e9e4e856183114bdf1b4c157
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2024-08-05 23:50:50 +0530

    Time: 5 ms (21.53%), Space: 43.1 MB (85.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,15 +3,15 @@ public class Solution {
         int[] result = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
-            result[i] = -1;
+            result[i] = -1;  //firstly it will be initialized by -1 an dif l;ater found then willn ereplced 
             boolean found = false;
             for (int j = 0; j < nums2.length; j++) {
                 int num2 = nums2[j]; // Declare num2 within the inner loop
-                if (found && nums1[i] < num2) {
+                if (found && nums1[i] < num2) {   //if the numbers are equal then only the comparison will be made 
                     result[i] = num2;
                     break;
                 }
-                if (nums1[i] == num2) {
+                if (nums1[i] == num2) {  // if only equal is found then only the boolean will be true 
                     found = true;
                 }
             }
