a62c7ad94bb732d82dc718939873b792c59e4f22
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-05-21 02:38:27 +0530

    Time: 0 ms (100%), Space: 43.3 MB (5.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,22 @@
 class Solution {
-    public void findsubsets(int[] nums, List<Integer> currentList, int i, List<List<Integer>> result) {
-        // Base case
-        if (i == nums.length) {
-            result.add(new ArrayList<>(currentList));
+    public List<List<Integer>> subsets(int[] nums) {
+        int i=0;
+        int n=nums.length;
+        List<Integer> ls =new ArrayList<>(); 
+        List<List<Integer>> result =new ArrayList<>();
+        print(i,ls,n, nums, result );
+        return result;
+        
+    }
+    public void print(int i, List<Integer> ls,int n, int[] nums, List<List<Integer>> result){
+        if(i>=n){
+            result.add(new ArrayList<>(ls));  // here understand  It means you're adding a copy of the current list ls into the final result list.
             return;
         }
+        ls.add(nums[i]);
+        print(i+1, ls,n,nums, result); 
+        ls.remove(ls.size()-1);                // here understand why size , coz this will ensure the last one is removed of the total size
+        print(i+1, ls,n,nums, result);
 
-        // Yes choice (include nums[i])
-        currentList.add(nums[i]);
-        findsubsets(nums, currentList, i + 1, result);
-
-        // Undo the choice for the no choice branch
-        currentList.remove(currentList.size() - 1);
-
-        // No choice (do not include nums[i])
-        findsubsets(nums, currentList, i + 1, result);
-    }
-
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        findsubsets(nums, new ArrayList<>(), 0, result);
-        return result;
     }
 }
