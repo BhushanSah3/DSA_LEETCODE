c8d88c4e3bb54526a5141f1cd41ba5850370f736
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-07-17 01:52:50 +0530

    Time: 4 ms (6.15%), Space: 50.1 MB (5.6%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int recur(int i,int per, int[] prices, int[][] dp){
+        if(i==prices.length) return 0;
+
+        if(dp[i][per]!=-1) return dp[i][per];
+
+        if(per==1){
+            dp[i][per]=Math.max( (-prices[i]+ recur(i+1,0, prices, dp) ), ( 0+ recur(i+1,1, prices, dp) ) );
+        }else{
+            dp[i][per] =Math.max(( prices[i]+ recur(i+1,1, prices, dp) ), (0+ recur(i+1,0, prices, dp)) );
+        }
+        return dp[i][per];
+
+
+    }
+
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][] dp =new int [n][2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return recur(0,1, prices,dp);
+        
+    }
+}
