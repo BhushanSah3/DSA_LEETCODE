d2603e32f5cbe79561ac200b8c9ddf40adad1421
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-07-17 12:08:29 +0530

    Time: 1 ms (78.37%), Space: 45.9 MB (59.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,44 @@
 class Solution {
-    public int recur(int i,int per, int[] prices, int[][] dp){
-        if(i==prices.length) return 0;
+    public int recur(int i, int per, int[] prices, int[][] dp) {
+        if (i == prices.length)
+            return 0;
 
-        if(dp[i][per]!=-1) return dp[i][per];
+        if (dp[i][per] != -1)
+            return dp[i][per];
 
-        if(per==1){
-            dp[i][per]=Math.max( (-prices[i]+ recur(i+1,0, prices, dp) ), ( 0+ recur(i+1,1, prices, dp) ) );
-        }else{
-            dp[i][per] =Math.max(( prices[i]+ recur(i+1,1, prices, dp) ), (0+ recur(i+1,0, prices, dp)) );
+        if (per == 1) {
+            dp[i][per] = Math.max((-prices[i] + recur(i + 1, 0, prices, dp)), (0 + recur(i + 1, 1, prices, dp)));
+        } else {
+            dp[i][per] = Math.max((prices[i] + recur(i + 1, 1, prices, dp)), (0 + recur(i + 1, 0, prices, dp)));
         }
         return dp[i][per];
-
 
     }
 
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int[][] dp =new int [n][2];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2 + 1];
+
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return recur(0,1, prices,dp);
+
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int per = 0; per <= 1; per++) {
+                if (per == 1) {
+                    dp[i][per] = Math.max((-prices[i] + dp[i+1][0]),
+                            (0 + dp[i+1][1]));
+                } else {
+                    dp[i][per] = Math.max((prices[i] + dp[i + 1][1]), (0 + dp[i+1][0]) );
+                }
+            }
         }
-        return recur(0,1, prices,dp);
-        
+
+        return dp[0][1];
+
     }
 }
