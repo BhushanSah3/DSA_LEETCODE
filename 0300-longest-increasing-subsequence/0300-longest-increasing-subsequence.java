2cc92962cf74ab2b1ef4c33a5ad4b8aa45fa0f47
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-07-24 01:21:09 +0530

    Time: 74 ms (25.35%), Space: 77.6 MB (16.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,43 @@
 class Solution {
-    public int recur(int i,int prev, int[] nums, int[][] dp){ 
-        if(i==nums.length) return 0;
+    public int recur(int i, int prev, int[] nums, int[][] dp) {
+        if (i == nums.length)
+            return 0;
 
-       if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        if (dp[i][prev + 1] != -1)
+            return dp[i][prev + 1];
 
-        int take=0;
-        int nottake=0+ recur(i+1,prev, nums, dp);
+        int take = 0;
+        int nottake = 0 + recur(i + 1, prev, nums, dp);
 
-        if(prev==-1 || nums[i]> nums[prev]){
-            take= 1+ recur(i+1,i, nums, dp);
+        if (prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + recur(i + 1, i, nums, dp);
         }
-         return dp[i][prev+1]= Math.max(take, nottake);
+        return dp[i][prev + 1] = Math.max(take, nottake);
 
     }
 
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[][]  dp =new int[n][n+1];
+        int n = nums.length;
+        //int[][] dp = new int[n][n + 1];
 
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+
+        // return recur(0,-1, nums, dp);
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+
+                int take = 0;
+                int nottake = 0 + dp[i + 1][prev+1];
+
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i + 1][i+1];
+                }
+                dp[i][prev + 1] = Math.max(take, nottake);
+            }
         }
-
-        return recur(0,-1, nums, dp);
+        return dp[0][0];
     }
 }
