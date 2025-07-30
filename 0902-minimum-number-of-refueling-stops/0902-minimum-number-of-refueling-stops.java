4d34edcdf9e7c2d88075d6ecb076ecc72743a173
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-07-31 02:37:48 +0530

    Time: 6 ms (10.44%), Space: 45.5 MB (11.98%) - LeetHub

--- 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.java
+++ 0902-minimum-number-of-refueling-stops/0902-minimum-number-of-refueling-stops.java
@@ -1,45 +1,23 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         int n = stations.length;
-        long[][] dp = new long[n + 1][n + 1];
-          
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+        long[] dp = new long[n + 1];
+        dp[0] = startFuel;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] >= stations[i][0]) {
+                    dp[j + 1] = Math.max(dp[j + 1], dp[j] + stations[i][1]);
+                }
+            }
         }
 
-        for (int stops = 0; stops <= n; stops++) {
-            long dist = recur(n, stops, startFuel, stations, dp);
-            if (dist >= target) {
-                return stops;
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] >= target) {
+                return i;
             }
         }
 
         return -1;
-    }
-
-    private long recur(int i, int stops, int startFuel, int[][] stations, long[][] dp) {
-        if (stops < 0) {
-            return Long.MIN_VALUE;
-        }
-
-        if (i == 0) {
-            if(stops == 0) return startFuel;
-            return Long.MIN_VALUE;
-        }
-
-        if (dp[i][stops] != -1) {
-            return dp[i][stops];
-        }
-
-        long nottake = recur(i - 1, stops, startFuel, stations, dp);
-
-        long take = Long.MIN_VALUE;
-        long reachbefore = recur(i - 1, stops - 1, startFuel, stations, dp);
-        
-        if (reachbefore >= stations[i - 1][0]) {
-            take = reachbefore + stations[i - 1][1];
-        }
-
-        return dp[i][stops] = Math.max(take, nottake);
     }
 }
