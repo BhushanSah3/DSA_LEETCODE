67200c6c68b9ac71a47027cf80d049c6b43caf97
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2025-09-20 02:44:19 +0530

    Time: 0 ms (100%), Space: 45.7 MB (68.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,22 +1,52 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int i, j;
-        int[] row=new int[matrix.length];
-        int[] colm=new int[matrix[0].length];
+        boolean rowzero = false, colzero = false;
+        int n = matrix.length;
+        int m = matrix[0].length;
 
-        for (i = 0; i < matrix.length; i++) {
-            for (j = 0; j < matrix[0].length; j++) {  
-                if (matrix[i][j] == 0 || matrix[i][j]==0) {
-                        row[i]=1;
-                        colm[j]=1;
-                    }
+        // 1. Check first row
+        for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                rowzero = true;
+                break;
+            }
+        }
+
+        // 2. Check first column
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                colzero = true;
+                break;
+            }
+        }
+
+        // 3. Mark rows & cols
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
-        for( i = 0 ;i<matrix.length;i++){
-            for (j = 0; j < matrix[0].length; j++) {  
-                if (row[i]==1 || colm[j]==1) {
+        }
+
+        // 4. Update cells
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-}}
+
+        // 5. Handle first row
+        if (rowzero) {
+            for (int j = 0; j < m; j++) matrix[0][j] = 0;
+        }
+
+        // 6. Handle first col
+        if (colzero) {
+            for (int i = 0; i < n; i++) matrix[i][0] = 0;
+        }
+    }
+}
