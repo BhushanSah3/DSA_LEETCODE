5053c3a2ffa6b5a37dbbdd3ff96219b244a56d11
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2026-01-15 00:28:22 +0530

    Time: 3 ms (82.32%), Space: 45.7 MB (47%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,41 +1,18 @@
 class Solution {
-    public int findpath(int i, int j, List<List<Integer>> triangle, int[][] dp) {
-        // if (i == triangle.size() || j == triangle.size())
-        //     return 0;
-        if (i == triangle.size())
-            return 0;
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int leftside = findpath(i + 1, j, triangle, dp);
-        int rightside = findpath(i + 1, j + 1, triangle, dp);
-
-        dp[i][j] = triangle.get(i).get(j) + Math.min(leftside, rightside);
-        return dp[i][j];
-    }
-
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
+        // TABULATION WITH 1D ARRAY
+        int n= triangle.size(); // this is the outer arraylist length remeber 
+        int[] dp= new int[n];
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        for(int i=0;i<n;i++){
+            dp[i]= triangle.get(n-1).get(i);
         }
-        // return findpath(0, 0, triangle,dp);
 
-        //TABULATION FROM HERE 
-        for (int j = 0; j < n; j++) {
-            dp[n - 1][j] = triangle.get(n - 1).get(j);
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                dp[i][j] = triangle.get(i).get(j)
-                        + Math.min(dp[i + 1][j], dp[i + 1][j + 1]);
-
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[j]=triangle.get(i).get(j)+Math.min(dp[j], dp[j+1]);
             }
         }
-        return dp[0][0];
-
+        return dp[0];
     }
 }
