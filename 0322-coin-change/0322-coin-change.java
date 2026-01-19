89d631752b422f4c4635e0c1f3bb12578696f17b
Bhushan Sah <125694860+BhushanSah3@users.noreply.github.com>
2026-01-20 02:24:35 +0530

    Time: 20 ms (37.91%), Space: 47.1 MB (28.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,37 +1,63 @@
 class Solution {
-    int inf=1000000007;
-    public int recur(int[] coins, int idx, int amount, int[][] dp){
+    int inf = 1000000007;
+
+    public int recur(int[] coins, int idx, int amount, int[][] dp) {
         if (idx == 0) {
-            if (amount % coins[0] == 0){
+            if (amount % coins[0] == 0) {
                 return amount / coins[0];
-            }     
-            else{
+            } else {
                 return inf;
-            }    
+            }
         }
 
-        if(dp[idx][amount]!=-1){
+        if (dp[idx][amount] != -1) {
             return dp[idx][amount];
         }
 
-        int nottake=0+ recur(coins, idx-1, amount,dp);
-        int take= inf;
+        int nottake = 0 + recur(coins, idx - 1, amount, dp);
+        int take = inf;
 
-        if(coins[idx]<= amount){
-            take= 1+ recur(coins,idx, amount-coins[idx],dp);
+        if (coins[idx] <= amount) {
+            take = 1 + recur(coins, idx, amount - coins[idx], dp);
         }
-        
-        return dp[idx][amount]=Math.min(take, nottake);
+
+        return dp[idx][amount] = Math.min(take, nottake);
     }
+
     public int coinChange(int[] coins, int amount) {
-        if(amount==0) return 0;
-        int n=coins.length;
-        int[][] dp= new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1); 
+        if (amount == 0)
+            return 0;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        
-        int ans= recur(coins,coins.length-1, amount,dp);
+
+        // int ans= recur(coins,coins.length-1, amount,dp);
+        // return ans >= inf ? -1 : ans;
+
+        //TABULATION STARTS HERE
+
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[0][t] = t / coins[0];
+            } else {
+                dp[0][t]=inf;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int nottake = 0 + dp[i - 1][t];
+                int take = inf;
+
+                if (coins[i] <= t) {
+                    take = 1 + dp[i][t-coins[i]];
+                }
+                dp[i][t]=Math.min(take, nottake);
+            }
+        }
+        int ans= dp[n-1][amount];
         return ans >= inf ? -1 : ans;
     }
 }
